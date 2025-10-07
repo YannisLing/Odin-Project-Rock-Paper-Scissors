727449e52fad218167f3503c0257bc518a8ebb15
YannisLing <3270614976@qq.com>
2025-10-07 16:01:26 +0800

    完成playround函数

--- script.js
+++ script.js
@@ -19,3 +19,45 @@ function getHumanChoice(){
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if(computerChoice.toLowerCase()){
+        console.log("draw");
+        return;
+    }
+    if(humanChoice.toLowerCase()==="rock"){
+        switch(computerChoice.toLowerCase()){
+            case "paper":
+                console.log("You lose! Paper beats Rock");
+                computerScore++;
+            case "scissors":
+                console.log("You win! Rock beats Scissors");
+                humanScore++;    
+        }
+    }
+    if(humanChoice.toLowerCase()==="scissors"){
+        switch(computerChoice.toLowerCase()){
+            case "paper":
+                console.log("You win! Scissors beats Paper");
+                computerScore++;
+            case "rock":
+                console.log("You lose! Rock beats Scissors");
+                humanScore++;    
+        }
+    }  
+    if(humanChoice.toLowerCase()==="paper"){
+        switch(computerChoice.toLowerCase()){
+            case "rock":
+                console.log("You win! Paper beats Rock");
+                computerScore++;
+            case "scissors":
+                console.log("You lose! Scissors beats Paper");
+                humanScore++;    
+        }
+    }        
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
