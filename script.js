0ca64819dd0b631878dc0493095aff90e2c3a7b5
YannisLing <3270614976@qq.com>
2025-10-07 15:31:35 +0800

    修正getComputerChoice函数,随即返回电脑选择

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function getComputerChoice(){
     let i = Math.floor(Math.random() * 3);//i可能取 0 1 2
     switch (i) {
         case 0:
-            return "rock";
+            return ("rock");
             break;
-        case 0:
-            return "paper";
+        case 1:
+            return ("paper");
             break;
-        case 0:
-            return "scissors"
+        case 2:
+            return ("scissors");
             break;
     }
 }
