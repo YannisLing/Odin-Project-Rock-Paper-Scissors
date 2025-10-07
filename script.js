96dce1a05ac40f5a6ba04045d6ef886838240ac9
YannisLing <3270614976@qq.com>
2025-10-07 15:25:04 +0800

    完成getComputerChoice函数,随即返回电脑选择

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice(){
+    let i = Math.floor(Math.random() * 3);//i可能取 0 1 2
+    switch (i) {
+        case 0:
+            return "rock";
+            break;
+        case 0:
+            return "paper";
+            break;
+        case 0:
+            return "scissors"
+            break;
+    }
+}
