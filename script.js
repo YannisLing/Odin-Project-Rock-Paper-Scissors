933388c5d4c47cd97287f233dfc59264361f289b
YannisLing <3270614976@qq.com>
2026-02-08 23:53:26 +0800

    完成

--- script.js
+++ script.js
@@ -1,82 +1,80 @@
-function getComputerChoice(){
-    let i = Math.floor(Math.random() * 3);//i可能取 0 1 2
-    switch (i) {
-        case 0:
-            return ("rock");
-            break;
-        case 1:
-            return ("paper");
-            break;
-        case 2:
-            return ("scissors");
-            break;
-    }
+const btnRock = document.querySelector('.rock');
+const btnPaper = document.querySelector('.paper');
+const btnScissors = document.querySelector('.scissors');
+let humanScore = 0;
+let computerScore = 0;
+const result1 = document.querySelector('.result1');
+const result2 = document.querySelector('.result2');
+
+function getComputerChoice() {
+  let i = Math.floor(Math.random() * 3);//i可能取 0 1 2
+  switch (i) {
+    case 0:
+      return ("rock");
+      break;
+    case 1:
+      return ("paper");
+      break;
+    case 2:
+      return ("scissors");
+      break;
+  }
 }
 
-function getHumanChoice(){
-    return prompt("what's your choice?");
-}
+btnPaper.addEventListener('click',()=>playRound('paper',getComputerChoice()));
+btnRock.addEventListener('click',()=>playRound('rock',getComputerChoice()));
+btnScissors.addEventListener('click',()=>playRound('scissors',getComputerChoice()));
 
 
 
 
-function playGame(){
-    function playRound(humanChoice, computerChoice) {
-    if(computerChoice.toLowerCase()===humanChoice.toLowerCase()){
-        console.log("draw");
-        console.log("Your Score: "+humanScore+"  Computer Score: "+computerScore);
+function playRound(humanChoice, computerChoice) {
+  if (computerChoice.toLowerCase() === humanChoice.toLowerCase()) {
+    result1.textContent="draw"
+    result2.textContent="Your Score: " + humanScore + "  Computer Score: " + computerScore;
+    
+    return;
+  }
+  if (humanChoice.toLowerCase() === "rock") {
+    switch (computerChoice.toLowerCase()) {
+      case "paper":
+        result1.textContent="You lose! Paper beats Rock";
+        computerScore++;
+        result2.textContent="Your Score: " + humanScore + "  Computer Score: " + computerScore;
+        return;
+      case "scissors":
+        result1.textContent="You win! Rock beats Scissors";
+        humanScore++;
+        result2.textContent="Your Score: " + humanScore + "  Computer Score: " + computerScore;
         return;
     }
-    if(humanChoice.toLowerCase()==="rock"){
-        switch(computerChoice.toLowerCase()){
-            case "paper":
-                console.log("You lose! Paper beats Rock");
-                computerScore++;
-                console.log("Your Score: "+humanScore+"  Computer Score: "+computerScore);
-                return;
-            case "scissors":
-                console.log("You win! Rock beats Scissors");
-                humanScore++;
-                console.log("Your Score: "+humanScore+"  Computer Score: "+computerScore);
-                return;    
-        }
+  }
+  if (humanChoice.toLowerCase() === "scissors") {
+    switch (computerChoice.toLowerCase()) {
+      case "paper":
+        result1.textContent="You win! Scissors beats Paper";
+        humanScore++;
+        result2.textContent="Your Score: " + humanScore + "  Computer Score: " + computerScore;
+        return;
+      case "rock":
+        result1.textContent="You lose! Rock beats Scissors";
+        computerScore++;
+        result2.textContent="Your Score: " + humanScore + "  Computer Score: " + computerScore;
+        return;
     }
-    if(humanChoice.toLowerCase()==="scissors"){
-        switch(computerChoice.toLowerCase()){
-            case "paper":
-                console.log("You win! Scissors beats Paper");
-                computerScore++;
-                console.log("Your Score: "+humanScore+"  Computer Score: "+computerScore);
-                return; 
-            case "rock":
-                console.log("You lose! Rock beats Scissors");
-                humanScore++; 
-                console.log("Your Score: "+humanScore+"  Computer Score: "+computerScore);   
-                return; 
-        }
-    }  
-    if(humanChoice.toLowerCase()==="paper"){
-        switch(computerChoice.toLowerCase()){
-            case "rock":
-                console.log("You win! Paper beats Rock");
-                computerScore++;
-                console.log("Your Score: "+humanScore+"  Computer Score: "+computerScore);
-                return; 
-            case "scissors":
-                console.log("You lose! Scissors beats Paper");
-                humanScore++;  
-                console.log("Your Score: "+humanScore+"  Computer Score: "+computerScore);  
-                return; 
-        }
-    }        
+  }
+  if (humanChoice.toLowerCase() === "paper") {
+    switch (computerChoice.toLowerCase()) {
+      case "rock":
+        result1.textContent="You win! Paper beats Rock";
+        humanScore++;
+        result2.textContent="Your Score: " + humanScore + "  Computer Score: " + computerScore;
+        return;
+      case "scissors":
+        result1.textContent="You lose! Scissors beats Paper";
+        computerScore++;
+        result2.textContent="Your Score: " + humanScore + "  Computer Score: " + computerScore;
+        return;
+    }
+  }
 }
-    let humanScore = 0;
-    let computerScore = 0;
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-}
-   
-playGame();
